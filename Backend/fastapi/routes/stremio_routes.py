284b866f5057d7a6b9862e0e8723bba9adecdb61
RHiNo410 <119166195+RHiNo410@users.noreply.github.com>
2026-08-08 09:04:40 +0530

    Update stremio_routes.py

--- Backend/fastapi/routes/stremio_routes.py
+++ Backend/fastapi/routes/stremio_routes.py
@@ -30,7 +30,7 @@ router = APIRouter(prefix="/stremio", tags=["Stremio Addon"])
 templates = Jinja2Templates(directory="Backend/fastapi/templates")
 
 #----- Addon configuration
-ADDON_NAME = "Telegram"
+ADDON_NAME = "Jerin"
 ADDON_VERSION = __version__
 PAGE_SIZE = 15
 
